920d383b97b8c87e047503df7b9f7ab63f5fbd89
mateoeulORT <48044168@est.ort.edu.ar>
2024-11-19 09:03:33 -0300

    arregle una cosita

--- Models/Carreras.cs
+++ Models/Carreras.cs
@@ -1,15 +1,9 @@
 public class Carreras
 {
-public class Carreras
-{
     public int Id { get; set; }
     public int IdUniversidad { get; set; }
     public string? Nombre { get; set; }
     public int Duracion { get; set; }
     public int Costo { get; set; }
     public string? Foto { get; set; }
-
-    // Nueva propiedad para asociar la universidad
-    public Universidades Universidad { get; set; }
-}
 }
