87b501ac99c9bc9aa78602fa27a7bb5ec6a56626
mateoeulORT <48044168@est.ort.edu.ar>
2025-03-28 12:13:46 -0300

    base de datos

--- obj/Debug/net8.0/Uni..AssemblyInfo.cs
+++ obj/Debug/net8.0/Uni..AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Uni.")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffe34132af2312b5451952981e06d6cba04fd478")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be53679b9be1247579a724ae940f5a6ead78bd1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Uni.")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Uni.")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
