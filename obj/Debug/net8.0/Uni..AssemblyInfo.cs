1912dcc1661526644b61f9a87817e272cd22544d
mateoeulORT <48044168@est.ort.edu.ar>
2025-04-25 12:12:12 -0300

    por las dudas

--- obj/Debug/net8.0/Uni..AssemblyInfo.cs
+++ obj/Debug/net8.0/Uni..AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Uni.")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87b501ac99c9bc9aa78602fa27a7bb5ec6a56626")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ea5387ef00605298ef58b02a7fc91063504a506")]
 [assembly: System.Reflection.AssemblyProductAttribute("Uni.")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Uni.")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
